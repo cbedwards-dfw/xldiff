f72f499892afd2090d0ff0863befe1ab57ee1235
cedwards-dfw <Collin.Edwards@dfw.wa.gov>
2024-06-03 15:53:09 -0400

    created  to streamline applying styles to collections of cells based on excel range nomenclature

---
+++ R/cells_stylize.R
@@ -0,0 +1,17 @@
+#' Apply style to worksheet based on one or more excel-style cell ranges
+#'
+#' @inheritParams add_cell_borders
+#' @param style `openxlsx` cell style, created with `openxlsx::createStyle()`
+#' @param stack Should style be appended to existing styles (`TRUE`) or replace existing styles (`FALSE`). Defaults to `TRUE`.
+#'
+#' @export
+#'
+cells_stylize <- function(wb, sheet, style, block.ranges, stack = TRUE){
+  cells.modify = do.call(rbind,
+                           purrr::map(block.ranges,
+                                      .f = cell_range_translate))
+  openxlsx::addStyle(wb, sheet = sheet, style = style,
+                     rows = cells.modify$row,
+                     cols = cells.modify$col,
+                     stack = stack, gridExpand = FALSE)
+}
