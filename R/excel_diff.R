6fba00f81d22c6462a4a1df5f97a9eb7ccb85912
Collin Edwards <collin.edwards@dfw.wa.gov>
2025-12-30 17:10:41 -0800

    excel_diff uses openxlsx2 but saves all sheets

--- R/excel_diff.R
+++ R/excel_diff.R
@@ -1,11 +1,8 @@
-
-
 #' Minimal spreadsheet comparison function
 #'
-#' Compares a single sheet between two files, supports providing additional formatting
-#' in the form of the optional `extra_format_fun` argument. For more complex use cases (e.g.,
-#' multiple sheet, pre-comparison formatting to compare only specific regions, etc) `excel_diff` can
-#' be used as a simple template for writing your own function. Small numeric differences between cell values can be the result of happenstance ("decimal dust") - the `digits.signif` and `proportional.diff` arguments can control behavior to ignore minor numeric differences.
+#' Compares a single sheet between two files, creating a new file that uses the formatting of
+#' the existing sheets (except highlighting) and highlights cells that differed.
+#' Small numeric differences between cell values can be the result of happenstance ("decimal dust") - the `digits.signif` and `proportional.diff` arguments can control behavior to ignore minor numeric differences.
 #'
 #' @param file.1 Filename (including path) for first file to compare
 #' @param file.2 Filename (including path) for second file to compare
@@ -14,92 +11,64 @@
 #' @param digits.signif Numeric, controls the amount of difference a number needs to have to be identified as differing between the two sheets. When `proportional.diff = TRUE`, defines proportional change that triggers a "diff" status, where the number is the number of digits of the proportion (2 = 1% difference, 3 = 0.1% difference, 4 = 0.01% difference). When `proportional.diff = FALSE`, defines the absolute difference that triggers a difference, in digits of round (1 = 0.1 difference, 2 = 0.01 difference, 3 = 0.001).
 #' @param proportional.diff Should minor differences between cells be judged on an absolute basis (`FALSE`) or a proportional basis (`TRUE`). `FALSE` is useful for identifying a level of decimal dust that we don't want to worry about when diff-ing (e.g., "If the differences is in the 1000ths place or smaller, I don't care"). `TRUE` is useful when a sheet contains values of varying magnitudes, as it allows specifying proprotional changes that should be ignored when diffing (e.g., "If the value changed by less than 0.1%, I don't care).
 #'
-#' @param extra_format_fun Optional function to apply additional formatting, allowing users to specify additional
-#' calls of `addStyle()` (or other openxslx functions, like setting column width). First argument must be the workbook
-#' object this function makes changes to; second argument must be the name of the worksheet this function makes
-#' changes to
-#' @param ... Additional arguments passed to `extra_format_fun`
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' filename.1 = "Documents/WDFW FRAM team work/NOF material/NOF 2024/FRAM/Chin1124.xlsx"
-#'filename.2 = "Documents/WDFW FRAM team work/NOF material/NOF 2024/NOF 2/Chin2524.xlsx"
+#' filename.1 = "Chin1124.xlsx"
+#' filename.2 = "Chin2524.xlsx"
 #'
-#'excel_diff(file.1 = filename.1,
+#' excel_diff(file.1 = filename.1,
 #'           file.2 = filename.2,
-#'           results.name = "Documents/WDFW FRAM team work/NOF material/NOF 2024/test1.xlsx",
+#'           results.name = "Chin1124 vs Chin 2524.xlsx",
 #'           sheet.name = "ER_ESC_Overview_New"
-#')
-#'
-#' ## create function to add in some additional formatting:
-#' extra_form_fun = function(wb, sheet){
-#'  ## add bold and increased size for the first two rows.
-#'  openxlsx::addStyle(wb, sheet,
-#'                     style = openxlsx::createStyle(fontSize = 16, textDecoration = "Bold"),
-#'                     rows = 1:2, cols = 1:8, gridExpand = TRUE,
-#'                     stack = TRUE)
-#'  ## add thin inner cell borders
-#'  add_cell_borders(wb, sheet,
-#'                   block.ranges = c("B3:H34") )
-#'  ## add thick outer borders
-#'  add_cell_borders(wb, sheet,
-#'                   block.ranges = c("A2", "B1:D2", "E1:H2",
-#'                                    "A3:A34", "B3:D34", "E3:H34",
-#'                                    "D36:H37"),
-#'                   border.thickness = "medium")
-#'}
-#'
-#'excel_diff(file.1 = filename.1,
-#'           file.2 = filename.2,
-#'           results.name = "Documents/WDFW FRAM team work/NOF material/NOF 2024/test2.xlsx",
-#'           sheet.name = "ER_ESC_Overview_New",
-#'           extra_format_fun = extra_form_fun
-#')
-#'}
-
-
+#' )
+#' }
 
 excel_diff = function(file.1, file.2, results.name, sheet.name,
                       digits.signif = 3,
-                      proportional.diff = TRUE,
-                      extra_format_fun = NULL, ...){
+                      proportional.diff = TRUE){
 
   if(!all(grepl(".xls.?$", c(file.1, file.2, results.name)))){
     cli::cli_abort("`file.1`, `file.2`, and `results.name` must end in `.xlsx` or `.xls`.")
   }
-  if(!is.null(extra_format_fun) & !is.function(extra_format_fun)){
-    cli::cli_abort("If provided, `extra_format_fun` must be a function.")
-  }
+
   if(length(sheet.name) == 1){
     sheet.name = c(sheet.name, sheet.name)
   }
 
-  f1 = readxl::read_excel(file.1, sheet = sheet.name[1], col_names = FALSE,
-                          .name_repair = "unique_quiet")
-  f2 = readxl::read_excel(file.2, sheet = sheet.name[2], col_names = FALSE,
-                          .name_repair = "unique_quiet")
+  wb <- openxlsx2::wb_load(file.1, sheet = sheet.name[1])
+  wb2 <- openxlsx2::wb_load(file.2, sheet = sheet.name[2])
 
-  #carry out sheet comparison
-  sheet.comp = sheet_comp(f1, f2, digits.signif = digits.signif, proportional.diff = proportional.diff)
+  f1 = openxlsx2::wb_to_df(wb,
+                           sheet = sheet.name[1],
+                           col_names = FALSE,
+                           na = NA)
 
-  ## create workbook
-  wb = openxlsx::createWorkbook()
-  ## add in our sheet, fill in with the comparison information
-  openxlsx::addWorksheet(wb, sheetName = sheet.name[1])
-  openxlsx::writeData(wb, sheet.name[1], x = sheet.comp$sheet.diff, colNames = FALSE, keepNA = FALSE)
+  f2 = openxlsx2::wb_to_df(wb2,
+                           sheet = sheet.name[2],
+                           col_names = FALSE,
+                           na = NA)
 
-  ## support adding in additional formatting
-  if(! is.null(extra_format_fun)){
-    extra_format_fun(wb, sheet.name[1], ...)
-  }
+  sheet.comp = sheet_comp(f1, f2,
+                          digits.signif = digits.signif,
+                          proportional.diff = proportional.diff)
 
-  ## highlight cells that changes
-  add_changed_formats(wb, cur.sheet = sheet.name[1], sheet.comp = sheet.comp)
+  all_dims <- dim(f1)
+  all_dims_a1 <-  openxlsx2::wb_dims(1:all_dims[1], 1:all_dims[2])
 
+  wb_new <- wb |>
+    openxlsx2::wb_add_fill(sheet = sheet.name[1],
+                dims = all_dims_a1,
+                color = NULL) |>
+    openxlsx2::wb_add_data(sheet = sheet.name[1],
+                x = sheet.comp$sheet.diff, na = "",
+                col_names = FALSE) |>
+    add_changed_formats(cur.sheet = sheet.name[1],
+                        sheet.comp = sheet.comp)
 
-  openxlsx::saveWorkbook(wb, file = results.name, overwrite = TRUE)
+  openxlsx2::wb_save(wb_new, file = results.name, overwrite = TRUE)
 }
 
 
--- R/sheet_comp.R
+++ R/sheet_comp.R
@@ -24,133 +24,165 @@
 #' \dontrun{
 #' ## using palmerpenguins data to simulate spreadsheets
 #' library(palmerpenguins)
-#' t1 = t2 = head(penguins)
+#' t1 <- t2 <- head(penguins)
 #' ## change island variable to characters for easier modification
-#' t2$island = t1$island = as.character(t1$island)
+#' t2$island <- t1$island <- as.character(t1$island)
 #' ## change several entries in the second version
-#' t2$island[3] = "Scotland"
-#' t2$flipper_length_mm[1] = 18
+#' t2$island[3] <- "Scotland"
+#' t2$flipper_length_mm[1] <- 18
 #' sheet_comp(t1, t2, digits.signif = 4)
 #' }
-sheet_comp = function(t1, t2, digits.signif = 4, proportional.diff = FALSE){
-  if(!(all(dim(t1) == dim(t2)))){
+sheet_comp <- function(t1, t2, digits.signif = 4, proportional.diff = FALSE) {
+  if (!(all(dim(t1) == dim(t2)))) {
     cli::cli_abort("Dataframes `t1` and `t2` must have the same dimensions")
   }
-  if(!is.numeric(digits.signif)){
+  if (!is.numeric(digits.signif)) {
     cli::cli_abort("`digits.signif` must be positive integer.")
   }
-  if(digits.signif<0){
+  if (digits.signif < 0) {
     cli::cli_abort("`digits.signif` must be positive integer")
   }
-  if(floor(digits.signif)!=digits.signif){
+  if (floor(digits.signif) != digits.signif) {
     cli::cli_abort("`digits.signif` must be positive integer.")
   }
 
 
 
-  mat1 = as.matrix(t1)
-  mat2 = as.matrix(t2)
+  mat1 <- as.matrix(t1)
+  mat2 <- as.matrix(t2)
 
   ## cut out extraneous NAs now
-  mat1 = mat1[1:max(which(!apply(mat1, 1, function(x){all(is.na(x))}))),]
-  mat2 = mat2[1:max(which(!apply(mat2, 1, function(x){all(is.na(x))}))),]
+  mat1 <- mat1[1:max(which(!apply(mat1, 1, function(x) {
+    all(is.na(x))
+  }))), ]
+  mat2 <- mat2[1:max(which(!apply(mat2, 1, function(x) {
+    all(is.na(x))
+  }))), ]
 
   ## translate NAs for better comparison
-  mat1[is.na(mat1)] = "**NA**"
-  mat2[is.na(mat2)] = "**NA**"
+  mat1[is.na(mat1)] <- "**NA**"
+  mat2[is.na(mat2)] <- "**NA**"
 
   ## find differences
-  mat.diff = (mat1 != mat2)
+  mat.diff <- (mat1 != mat2)
   ## find which values can be treated as numbers and which can't
-  mat.numbers = suppressWarnings(!is.na(as.numeric(mat1)) & !is.na(as.numeric(mat2)))
-  mat.numbers.percents = suppressWarnings(!is.na(as.numeric(gsub("%$", "", mat1))) &
-                                            !is.na(as.numeric(gsub("%$", "", mat2))))
-  mat.numbers.percents = mat.numbers.percents & !mat.numbers
-  comparify_nums = function(x){
-    as.numeric(x) + sign(as.numeric(x))/1000
+  mat.numbers <- suppressWarnings(!is.na(as.numeric(mat1)) & !is.na(as.numeric(mat2)))
+  mat.numbers.percents <- suppressWarnings(!is.na(as.numeric(gsub("%$", "", mat1))) &
+    !is.na(as.numeric(gsub("%$", "", mat2))))
+  mat.numbers.percents <- mat.numbers.percents & !mat.numbers
+  comparify_nums <- function(x) {
+    as.numeric(x) + sign(as.numeric(x)) / 1000
   }
 
 
-  if(proportional.diff){
-    mat.denominator.zero = suppressWarnings(!is.na(as.numeric(mat1)) &
-                                              !is.na(as.numeric(mat2)) &
-                                              mat1 == 0)
-    mat.diff[mat.numbers] =
-      suppressWarnings(round(comparify_nums(mat2[mat.numbers]) /
-                               comparify_nums(mat1[mat.numbers]),
-                             digits.signif) != 1)
-    mat.diff[mat.numbers.percents] = suppressWarnings(
-      round(comparify_nums(gsub("%$", "", mat2)[mat.numbers.percents]) /
-              comparify_nums(gsub("%$", "", mat1)[mat.numbers.percents]),
-            digits.signif) != 1)
+  if (proportional.diff) {
+    mat.denominator.zero <- suppressWarnings(!is.na(as.numeric(mat1)) &
+      !is.na(as.numeric(mat2)) &
+      mat1 == 0)
+    mat.diff[mat.numbers] <-
+      suppressWarnings(round(
+        comparify_nums(mat2[mat.numbers]) /
+          comparify_nums(mat1[mat.numbers]),
+        digits.signif
+      ) != 1)
+    mat.diff[mat.numbers.percents] <- suppressWarnings(
+      round(
+        comparify_nums(gsub("%$", "", mat2)[mat.numbers.percents]) /
+          comparify_nums(gsub("%$", "", mat1)[mat.numbers.percents]),
+        digits.signif
+      ) != 1
+    )
     # handling cases when denominator is 0
-     mat.denominator.zero = suppressWarnings(!is.na(as.numeric(mat1)) &
-                                               !is.na(as.numeric(mat2)) &
-                                               mat1 == 0)
-     mat.diff[mat.denominator.zero] =
-       round(as.numeric(mat2[mat.denominator.zero]), digits.signif) != 0
-     ## handles cases when denominator is 0 and %s.
-     mat.denominator.zero =
-
-       suppressWarnings(!is.na(as.numeric(gsub("%$", "", mat1))) &
-                        !is.na(as.numeric(gsub("%$", "", mat2))) &
-                        !mat.numbers &
-                        as.numeric(gsub("%$", "", mat1) != 0)
-
-     )
-     mat.diff[mat.denominator.zero] =
-       round(as.numeric(gsub("%$", "", mat2)[mat.denominator.zero]), digits.signif) == 0
-
+    mat.denominator.zero <- suppressWarnings(!is.na(as.numeric(mat1)) &
+      !is.na(as.numeric(mat2)) &
+      mat1 == 0)
+    mat.diff[mat.denominator.zero] <-
+      round(as.numeric(mat2[mat.denominator.zero]), digits.signif) != 0
+    ## handles cases when denominator is 0 and %s.
+    mat.denominator.zero <-
+      suppressWarnings(!is.na(as.numeric(gsub("%$", "", mat1))) &
+        !is.na(as.numeric(gsub("%$", "", mat2))) &
+        !mat.numbers &
+        as.numeric(gsub("%$", "", mat1) != 0))
+    mat.diff[mat.denominator.zero] <-
+      round(as.numeric(gsub("%$", "", mat2)[mat.denominator.zero]), digits.signif) == 0
   } else {
     ## for numeric entries, update mat.diff with rounded comparison
-    mat.diff[mat.numbers] = round(as.numeric(mat1[mat.numbers]), digits.signif) !=
+    mat.diff[mat.numbers] <- round(as.numeric(mat1[mat.numbers]), digits.signif) !=
       round(as.numeric(mat2[mat.numbers]), digits.signif)
 
-    mat.diff[mat.numbers.percents] = suppressWarnings(
+    mat.diff[mat.numbers.percents] <- suppressWarnings(
       round(as.numeric(gsub("%$", "", mat1)[mat.numbers.percents]), digits.signif) !=
         round(as.numeric(gsub("%$", "", mat2)[mat.numbers.percents]), digits.signif)
     )
   }
 
   ## find indices for changes that are text and are numeric
-  ind.diff.text = which(mat.diff & !mat.numbers)
-  ind.diff.num = which(mat.diff & mat.numbers)
-  ind.diff.num.percents = which(mat.diff & mat.numbers.percents)
+  ind.diff.text <- which(mat.diff & !mat.numbers)
+  ind.diff.num <- which(mat.diff & mat.numbers)
+  ind.diff.num.percents <- which(mat.diff & mat.numbers.percents)
 
-  mat.new = mat1
-  mat.new[ind.diff.text] = paste0(mat1[ind.diff.text]," --> ",
-                                  mat2[ind.diff.text])
-  mat.new[ind.diff.num] = paste0(signif(as.numeric(mat1[ind.diff.num]), digits.signif)," --> ",
-                                 signif(as.numeric(mat2[ind.diff.num]), digits.signif))
-  mat.new[ind.diff.num.percents] = suppressWarnings(
-    paste0(signif(as.numeric(gsub("%$", "", mat1)[ind.diff.num.percents]),
-                  digits.signif),"% --> ",
-           signif(as.numeric(as.numeric(gsub("%$", "", mat2))[ind.diff.num.percents]),digits.signif), "%")
+  mat.new <- mat1
+  mat.new[ind.diff.text] <- paste0(
+    mat1[ind.diff.text], " --> ",
+    mat2[ind.diff.text]
   )
-  mat.new[mat.new == "**NA**"] = NA
+  if (proportional.diff) {
+    mat.new[ind.diff.num] <- paste0(
+      signif(as.numeric(mat1[ind.diff.num]), digits.signif + 2), " --> ",
+      signif(as.numeric(mat2[ind.diff.num]), digits.signif + 2)
+    )
+    mat.new[ind.diff.num.percents] <- suppressWarnings(
+      paste0(
+        signif(
+          as.numeric(gsub("%$", "", mat1)[ind.diff.num.percents]),
+          digits.signif + 2
+        ), "% --> ",
+        signif(as.numeric(as.numeric(gsub("%$", "", mat2))[ind.diff.num.percents]), digits.signif + 2), "%"
+      )
+    )
+  } else {
+    mat.new[ind.diff.num] <- paste0(
+      signif(as.numeric(mat1[ind.diff.num]), digits.signif), " --> ",
+      signif(as.numeric(mat2[ind.diff.num]), digits.signif)
+    )
+    mat.new[ind.diff.num.percents] <- suppressWarnings(
+      paste0(
+        signif(
+          as.numeric(gsub("%$", "", mat1)[ind.diff.num.percents]),
+          digits.signif
+        ), "% --> ",
+        signif(as.numeric(as.numeric(gsub("%$", "", mat2))[ind.diff.num.percents]), digits.signif), "%"
+      )
+    )
+  }
+  mat.new[mat.new == "**NA**"] <- NA
 
   ## store matrices for style differences
-  mat.diff.text = mat.diff & !mat.numbers
-  mat.diff.decrease = mat.diff.increase = matrix(FALSE,
-                                                 nrow = nrow(mat1),
-                                                 ncol = ncol(mat1))
-  mat.diff.decrease[ind.diff.num] = mat1[ind.diff.num] > mat2[ind.diff.num]
-  mat.diff.increase[ind.diff.num] = mat1[ind.diff.num] < mat2[ind.diff.num]
+  mat.diff.text <- mat.diff & !mat.numbers
+  mat.diff.decrease <- mat.diff.increase <- matrix(FALSE,
+    nrow = nrow(mat1),
+    ncol = ncol(mat1)
+  )
+  mat.diff.decrease[ind.diff.num] <- mat1[ind.diff.num] > mat2[ind.diff.num]
+  mat.diff.increase[ind.diff.num] <- mat1[ind.diff.num] < mat2[ind.diff.num]
 
-  if(length(ind.diff.num.percents)>0){
-    mat.diff.decrease[ind.diff.num.percents] = gsub("%$", "", mat1[ind.diff.num.percents]) >
+  if (length(ind.diff.num.percents) > 0) {
+    mat.diff.decrease[ind.diff.num.percents] <- gsub("%$", "", mat1[ind.diff.num.percents]) >
       gsub("%$", "", mat2[ind.diff.num.percents])
-    mat.diff.increase[ind.diff.num.percents] = gsub("%$", "", mat1[ind.diff.num.percents]) <
+    mat.diff.increase[ind.diff.num.percents] <- gsub("%$", "", mat1[ind.diff.num.percents]) <
       gsub("%$", "", mat2[ind.diff.num.percents])
   }
   ## clear extraneous NAs from bottom of each matrix
 
 
-  return(list(sheet.diff = mat.new,
-              mat.changed = mat.diff,
-              mat.diff.text = mat.diff.text,
-              mat.diff.decrease = mat.diff.decrease,
-              mat.diff.increase = mat.diff.increase))
+  return(list(
+    sheet.diff = mat.new,
+    mat.changed = mat.diff,
+    mat.diff.text = mat.diff.text,
+    mat.diff.decrease = mat.diff.decrease,
+    mat.diff.increase = mat.diff.increase
+  ))
 }
 
 #' Stripped down version of sheet_comp
@@ -159,23 +191,27 @@ sheet_comp = function(t1, t2, digits.signif = 4, proportional.diff = FALSE){
 #'
 #' @return logical matrix
 #'
-sheet_comp_basic <-  function(t1, t2){
-  if(!(all(dim(t1) == dim(t2)))){
+sheet_comp_basic <- function(t1, t2) {
+  if (!(all(dim(t1) == dim(t2)))) {
     cli::cli_abort("Dataframes `t1` and `t2` must have the same dimensions")
   }
 
-  mat1 = as.matrix(t1)
-  mat2 = as.matrix(t2)
+  mat1 <- as.matrix(t1)
+  mat2 <- as.matrix(t2)
 
   ## cut out extraneous NAs now
-  mat1 = mat1[1:max(which(!apply(mat1, 1, function(x){all(is.na(x))}))),]
-  mat2 = mat2[1:max(which(!apply(mat2, 1, function(x){all(is.na(x))}))),]
+  mat1 <- mat1[1:max(which(!apply(mat1, 1, function(x) {
+    all(is.na(x))
+  }))), ]
+  mat2 <- mat2[1:max(which(!apply(mat2, 1, function(x) {
+    all(is.na(x))
+  }))), ]
 
   ## translate NAs for better comparison
-  mat1[is.na(mat1)] = "**NA**"
-  mat2[is.na(mat2)] = "**NA**"
+  mat1[is.na(mat1)] <- "**NA**"
+  mat2[is.na(mat2)] <- "**NA**"
 
   ## find differences
-  mat.diff = (mat1 != mat2)
+  mat.diff <- (mat1 != mat2)
   return(mat.diff)
 }
